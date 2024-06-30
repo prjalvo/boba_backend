4612a81c699feb305e20d0f589f5adf00eb2a29b
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-30 15:40:22 -0300

    Update filiais.controller.js

--- src/api/resources/filiais/filiais.controller.js
+++ src/api/resources/filiais/filiais.controller.js
@@ -9,62 +9,23 @@ import util from 'util';
 
 
 
-var JWTSign = function (user, date) {
-    return JWT.sign({
-        iss: config.name,
-        sub: user.id,
-        iam : user.type,
-        iat: date.getTime(),
-        exp: new Date().setMinutes(date.getMinutes() + 480)
-    }, config.secret);
-}
-
-function generateOtp() {
-    let token = speakeasy.totp({
-        secret: process.env.OTP_KEY,
-        encoding: 'base32',
-        step: (30 - Math.floor((new Date().getTime() / 1000.0 % 30)))
-    });
-    return token;
-}
-
-function verifyOtp(token) {
-    let expiry = speakeasy.totp.verify({
-        secret: process.env.OTP_KEY,
-        encoding: 'base32',
-        token: token,
-        step: (30 - Math.floor((new Date().getTime() / 1000.0 % 30))),
-        window: 0
-    });
-    return expiry
-}
-
-
 export default {
-    async addUser(req, res, next) {
-        const { firstName, email, phone, id_cargo,id_distrito,id_area,id_setor,status, verify, role ,password,url_file,lider_celula } = req.body;
-        var passwordHash = bcrypt.hashSync(password);
-        var token = generateOtp();
-        var otp = verifyOtp(token);
-        db.user.findOne({ where: { email: email }, paranoid: false })
+    async addFiliais(req, res, next) {
+        const { nome, depositante,cd_estabelecimento,api_key,api_secret,logistica,processar,cnpj } = req.body;
+        db.filiais.findOne({ where: { nome: nome }, paranoid: false })
             .then(find => {
                 if (find) {
-                    throw new RequestError('Email is already in use', 409);
+                    throw new RequestError('Filiasl já cadastrada', 409);
                 }
-                return db.user.create({
-                    firstName: firstName,                    
-                    email: email,
-                    phone: phone,               
-                    id_cargo: id_cargo,
-                    id_distrito: id_distrito,
-                    id_area: id_area,
-                    id_setor: id_setor,
-                    status: status,                    
-                    verify: verify,
-                    role: role,
-                    password: passwordHash,
-                    url_file:url_file,
-                    lider_celula:lider_celula
+                return db.filiais.create({
+                    nome: nome,                    
+                    depositante: depositante,                      
+                    cd_estabelecimento: cd_estabelecimento,
+                    api_key: api_key,
+                    api_secret: api_secret,
+                    logistica: logistica,
+                    processar: processar,                    
+                    cnpj: cnpj
                 })
 
             })
@@ -82,11 +43,11 @@ export default {
             })
     },
 
-    async findUser(req,res,next){
-        db.user.findOne({ attributes:["firstName","lastName"], where: { email: req.query.email }, paranoid: false })
-        .then(user => {
-            if (user) {
-                return res.status(200).json({ success: true, data:user});
+     async getAllFiliais(req,res,next){
+        db.filiais.findAll({ })
+        .then(filiais => {
+            if (filiais) {
+                return res.status(200).json({ success: true, data:filiais});
             }
             else
                 res.status(500).json({ 'success': false });
@@ -97,50 +58,23 @@ export default {
         })
     },
 
-     async getAllUserList(req,res,next){
-        db.user.findAll({
-           include: [{ model: db.cargo, attributes: ["id", "descricao"]},
-                      { model: db.areas,as:"user_area",attributes: ["id", "descricao","tipo"]},
-                      { model: db.areas,as:"user_setor",attributes: ["id", "descricao","tipo"]},
-                      { model: db.areas,as:"user_distrito",attributes: ["id", "descricao","tipo"]}]
-        })
-        .then(user => {
-            if (user) {
-                return res.status(200).json({ success: true, data:user});
-            }
-            else
-                res.status(500).json({ 'success': false });
-        })
-        .catch(err => {
-            console.log(err)
-            next(err);
-        })
-    },
-
-     async userUpdate(req,res,next){
-        const { id, firstName, email, phone, id_cargo,id_distrito,id_area,id_setor, status, verify, role ,password,url_file,lider_celula } = req.body;
-        if (password !== null) {
-          var passwordHash = bcrypt.hashSync(password);
-        }    
-        db.user.findOne({ where: { id: id }, paranoid: false })
+     async filiaisUpdate(req,res,next){
+        const { id, nome, depositante,cd_estabelecimento,api_key,api_secret,logistica,processar,cnpj } = req.body;
+        
+        db.filiais.findOne({ where: { id: id }, paranoid: false })
             .then(user => {
                 if (!user) {
                     throw new RequestError('User is not found', 409);
                 }
-                return db.user.update({
-                    firstName: firstName ? firstName: user.firstName,
-                    phone: phone ? phone: user.phone,
-                    email: email ? email: user.email,
-                    password: password ? passwordHash: user.passwordHash,              
-                    role: role ? role: user.role,
-                    verify : verify? verify: user.verify,
-                    id_cargo: id_cargo ? id_cargo : user.id_cargo,
-                    id_distrito: id_distrito ? id_distrito : user.id_distrito,
-                    id_area: id_area ? id_area : user.id_area,
-                    id_setor: id_setor ? id_setor : user.id_setor,                    
-                    status: status,
-                    url_file: url_file ? url_file : user.url_file,
-                    lider_celula: lider_celula ? lider_celula : user.lider_celula
+                return db.filiais.update({
+                    nome: nome ? nome : filiais.nome,                    
+                    depositante: depositante ? depositante : filiais.depositante,                      
+                    cd_estabelecimento: cd_estabelecimento ? cd_estabelecimento : filiais.cd_estabelecimento,
+                    api_key: api_key ? api_key : filiais.api_key,
+                    api_secret: api_secret ? api_secret : filiais.api_secret,
+                    logistica: logistica ? logistica : filiais.logistica,
+                    processar: processar ? processar : filiais.processar,                    
+                    cnpj: cnpj ? cnpj : filiais.cnpj                     
                 }, { where: { id: id } })
             })
             .then(user => {
@@ -155,24 +89,11 @@ export default {
                 next(err);
             })
     },
-
-    async login(req, res, next) {
-        var date = new Date();
-        var token = JWTSign(req.user, date,{expiresIn: '12h' 
-                         });
-        res.cookie('XSRF-token',     token, {
-            expire: new Date().setMinutes(date.getMinutes() + 480),
-            httpOnly: true, secure: config.secure
-        });
-        
-        return res.status(200).json({ success: true ,token,role: req.user.role,firstName: req.user.firstName,id_lider: req.user.id});
-    },
-
-     async deleteUserList(req, res, next) {
-        db.user.findOne({ where: { id: req.body.id} })
+      async deleteFiliais(req, res, next) {
+        db.filiais.findOne({ where: { id: req.body.id} })
             .then(data => {
                 if (data) {
-                    return db.user.destroy({ where: { id: req.body.id } }).then(r => [r, data])
+                    return db.filiais.destroy({ where: { id: req.body.id } }).then(r => [r, data])
                 }
                 throw new RequestError('User is not found', 409)
             })
@@ -181,88 +102,5 @@ export default {
             }).catch(err => {
                 next(err)
             })
-    },
-
-   async forgotpassword(req,res,next) {
-      const { email } = req.body;  
-      const user = await db.user.findOne({ where: { email: email} })
-      if (!user) {
-        return res.status(404).json({ error: 'Usuário não encontrado' });
-      }    
-      // Gerar o token de redefinição de senha
-      const token = JWT.sign({ email }, process.env.OTP_KEY, { expiresIn: '1h' });
-    
-      // Enviar e-mail com o link de redefinição de senha
-      //sendPasswordResetEmail(email, token);    
-       const transporter = nodemailer.createTransport({
-          // Configurações do seu serviço de e-mail
-           host: 'smtp.gmail.com',
-           port: '587',
-          auth: {
-            user: 'prjalvo@gmail.com',
-            pass: 'cmsdrbftcrqbvuwn'
-          },
-            tls: {rejectUnauthorized: false},
-      });
-
-      const resetLink = 'https://ibaredeverde.app.br/reset-password?token=' + token
-    
-      const mailOptions = {
-                from: 'prjalvo@gmail.com',
-                to: email,
-                subject: 'Redefinição de Senha',
-                text: 'Para redefinir sua senha, clique neste link: ' + resetLink
-       };
-    
-      transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-              res.json({ message: error } );
-       } else {
-               res.json({ message: 'Token de redefinição de senha enviado por e-mail: ' + email });
-      }
-     });  
-     
-  },    
-  async resetpassword(req,res,next) {
-            const { token, password } = req.body;   
-            const verifyAsync = util.promisify(JWT.verify);              
-            // Verificar se o token é válido
-            try {  
-                    const decoded = await verifyAsync(token,process.env.OTP_KEY); 
-                      // Hash da nova senha
-                    const hashedPassword = bcrypt.hashSync(password);         
-                    // Verificar se o email do token está associado a uma conta existente
-                    const user = await db.user.findOne({ where: { email: decoded.email } } );
-                    if (!user) {
-                        return res.status(404).json({ error: 'Usuário não encontrado' });
-                    } 
-                    else
-                    {
-                        db.user.update(
-                        { password: hashedPassword ? hashedPassword : user.password },
-                        { where: { email: decoded.email } }
-                        );
-                
-                      // Retornar uma resposta de sucesso
-                      return res.status(200).json({ message: 'Senha atualizada com sucesso' });
-                    }
-              } catch (error) {
-                  // Erro ao verificar o token
-                  if (error.name === 'TokenExpiredError') {
-                      // O token expirou
-                     return res.status(200).json( { message: 'O token expirou.' } );
-                  } else if (error.name === 'JsonWebTokenError') {
-                    // O token é inválido ou malformado
-                    return res.status(200).json( { message: 'O token é inválido ou malformado.' } );
-                  } else {
-                    // Outro tipo de erro
-                    return res.status(500).json( { message: 'Ocorreu um erro ao verificar o token: ' + error.message } );
-                  }
-            }            
-   },
-
+    }
 }
-
-
-
-
