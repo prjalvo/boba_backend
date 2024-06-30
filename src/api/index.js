919b116982fc0a3a7ea07476d0b375cd9580429e
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-30 15:26:52 -0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { authRouter } from './resources/auth/index.js'
-
+import { filiaisRouter } from './resources/filiais/index.js'
  
 export const restRouter = express.Router();
 restRouter.use('/auth', authRouter);
+restRouter.use('/filiais', filiaisRouter);
 
 
