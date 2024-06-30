e7d6d19d452a3e80a641169caed7ed2993ae4838
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-30 12:15:07 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -11,7 +11,7 @@ export default {
      * Here you may specify the host address of database which will be
      * used for connection
      */
-    host  : process.env.DB_HOST || "postgresql://dpg-cq0momuehbks73ee6sjg-a.oregon-postgres.render.com",
+    host  : process.env.DB_HOST || "dpg-cq0momuehbks73ee6sjg-a.oregon-postgres.render.com",
 
     
     /**
