2dbe804283c97b843403246250df8c1b4f66628d
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-30 15:30:37 -0300

    Update filiais.router.js

--- src/api/resources/filiais/filiais.router.js
+++ src/api/resources/filiais/filiais.router.js
@@ -1,15 +1,13 @@
 import express from 'express';
-import authController from './auth.controller.js';
+import filiaisController from './filiais.controller.js';
 import { localStrategy , jwtStrategy} from '../../../middleware/strategy.js';
 import { sanitize } from '../../../middleware/sanitizer.js';
 import { validateBody, schemas } from '../../../middleware/validator.js';
 
-export const authRouter = express.Router();
-authRouter.route('/register').post(authController.addUser);
-authRouter.route('/user/getAllUserList').get(sanitize(), jwtStrategy, authController.getAllUserList);
-authRouter.route('/user/update').post(jwtStrategy, authController.userUpdate);
-authRouter.route('/user/delete').post(sanitize(), jwtStrategy, authController.deleteUserList);
-authRouter.route('/getUserByEmailId').get( authController.findUser);
-authRouter.route('/rootLogin').post(sanitize(),validateBody(schemas.loginSchema),localStrategy, authController.login);
-authRouter.route('/forgotpassword').post(authController.forgotpassword);
-authRouter.route('/resetpassword').post(authController.resetpassword);
+export const filiaisRouter = express.Router();
+authRouter.route('/register').post(filiaisController.addFiliais);
+authRouter.route('/getAllFiliais').get(sanitize(), jwtStrategy, filiaisController.getAllFiliais;
+authRouter.route('/update').post(jwtStrategy, filiaisController.filiaisUpdate);
+authRouter.route('/delete').post(sanitize(), jwtStrategy, filiaisController.deleteFiliais);
+
+
