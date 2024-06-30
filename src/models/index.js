282834de960a1a4cec42e6a737bcf49a537d4738
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-30 12:22:21 -0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -15,6 +15,7 @@ let sequelize = new Sequelize(config.database, config.username, config.password,
     host: config.host,
     port: config.port,
     dialect: config.connection,
+    dialectOptions: config.dialectOptions
     logging: false,
     underscored: true,
     pool: {
