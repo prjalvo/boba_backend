70881ebc382641970acb8db792c26af12468c372
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-06-30 22:29:57 -0300

    Update Filiais.cjs

--- src/models/Filiais.cjs
+++ src/models/Filiais.cjs
@@ -1,15 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const filiais = sequelize.define('filiais', {
-    id: {type: DataTypes.INTEGER,primaryKey: true},
+    cd_estabelecimento: {type: DataTypes.INTEGER,primaryKey: true},
     nome: DataTypes.STRING,
-    depositante: DataTypes.STRING,
-    cd_estabelecimento: DataTypes.INTEGER,
+    depositante: DataTypes.STRING,    
     api_key: DataTypes.STRING,
-	  api_secret: DataTypes.STRING,
-	  logistica: DataTypes.STRING,
-	  processar: DataTypes.STRING,
-	  cnpj: DataTypes.STRING,  
+    api_secret: DataTypes.STRING,
+    logistica: DataTypes.STRING,
+    processar: DataTypes.STRING,
+    cnpj: DataTypes.STRING,  
     createdAt: DataTypes.DATE, 
     updatedAt: DataTypes.DATE    
   }, {});
