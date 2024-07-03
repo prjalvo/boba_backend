fd87313a8c15179ef663f4fdaaacbff9ab3f8b8e
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-07-03 18:22:54 -0300

    Update produtos.router.js

--- src/api/resources/produtos/produtos.router.js
+++ src/api/resources/produtos/produtos.router.js
@@ -5,6 +5,6 @@ import { sanitize } from '../../../middleware/sanitizer.js';
 import { validateBody, schemas } from '../../../middleware/validator.js';
 
 export const produtosRouter = express.Router();
-produtosRouter.route('/register').post(jwtStrategy,produtosController.addProdutos);
+produtosRouter.route('/register').post(produtosController.addProdutos);
 produtosRouter.route('/getallprodutos').get(jwtStrategy, produtosController.getallprodutos);
 produtosRouter.route('/getallprodutosid').get(jwtStrategy, produtosController.getallprodutosid);
