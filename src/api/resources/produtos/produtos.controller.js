3ccd2280e44bdf075084e91b306541a905d3da22
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-07-03 19:09:45 -0300

    Update produtos.controller.js

--- src/api/resources/produtos/produtos.controller.js
+++ src/api/resources/produtos/produtos.controller.js
@@ -145,7 +145,7 @@ export default {
     },
 
     async getallprodutosid(req,res,next){
-        db.produtos.findOne({ where: { codigo_produto: req.query.codigo_produto }, paranoid: false })
+        db.produtos.findOne({ where: { ean: req.query.ean }, paranoid: false })
         .then(produto => {
             if (produto) {
                 return res.status(200).json({ success: true, data:produto});
