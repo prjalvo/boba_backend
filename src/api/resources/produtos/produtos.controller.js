a6e437c3351f2a1d2ba0ee166f071e3894ce82cf
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-07-20 16:03:06 -0300

    Update produtos.controller.js

--- src/api/resources/produtos/produtos.controller.js
+++ src/api/resources/produtos/produtos.controller.js
@@ -4,147 +4,147 @@ import nodemailer from 'nodemailer';
 import config from '../../../config/app.js';
 import bcrypt from 'bcrypt-nodejs';
 import speakeasy from 'speakeasy';
-import { validateEmail } from './../../../functions.js'
+import { validateEmail } from './../../../functions.js';
 import util from 'util';
-import axios from 'axios'
+import axios from 'axios';
 
-async function truncateprodutos(cd_estabelecimento,res, next) {
-  return db.produtos.destroy({ where: { cd_estabelecimento:cd_estabelecimento } })           
-  .then(re => {
-      return res.status(200).json({ 'status': "Produto detelado" });
-  }).catch(err => {
-      next(err)
-  })
+async function truncateprodutos(cd_estabelecimento, res, next) {
+  try {
+    await db.produtos.destroy({ where: { cd_estabelecimento } });
+    return res.status(200).json({ 'status': "Produto deletado" });
+  } catch (err) {
+    next(err);
+  }
 }
 
-export default {     
-      async InserirProdutos(req,res,next){
-           try {
-            const { cd_estabelecimento } = req.body;
-            truncateprodutos(cd_estabelecimento);            
-            console.log("Estabelecimento: ",cd_estabelecimento)
-            const produtos = await this.listarProdutos(req.body);  
-            //await listarProdutos(cd_estabelecimento) 
-            truncateprodutos("Produtos Listados");  
-            for (const produto of produtosall) {
-              await new Promise(resolve => {
-                setTimeout(() => {
-                  this.addProdutos(produto);
-                  resolve();
-                }, 1000); 
-              });
-            }        
-            res.status(200).json({ success: true, message: 'Produtos adicionados com sucesso.' });
-          } catch (error) {
-            console.error('Erro ao listar produtos:', error);
-            res.status(500).json({ success: false, message: 'Erro ao inserir produtos.' });
-          }
-        },    
+export default {
+  async InserirProdutos(req, res, next) {
+    try {
+      const { cd_estabelecimento } = req.body;
+      await this.truncateprodutos(cd_estabelecimento, res, next);            
+      console.log("Estabelecimento: ", cd_estabelecimento);
+      const produtos = await this.listarProdutos(req.body, next);  
+      await this.truncateprodutos("Produtos Listados", res, next);  
 
-  async listarProdutos(req,res,next) {
-  try {
-    const { cd_estabelecimento } = req.body;
-    let pagina = 1;
-    const registrosPorPagina = 1000;
-    let totalDeRegistros = 0;
-    let todosProdutos = [];
-    let app_key = "";
-    let app_secret = "";
-    console.log('Chaves', app_key, app_secret); 
-    if (cd_estabelecimento === 15) {
-      app_key = process.env.ApiKey_SP;
-      app_secret = process.env.ApiSecret_SP;
-    } else if (cd_estabelecimento === 5) {
-      app_key = process.env.ApiKey_ES;
-      app_secret = process.env.ApiSecret_ES;
-    } else {
-      app_key = process.env.ApiKey_RJ;
-      app_secret = process.env.ApiSecret_RJ;
+      for (const produto of produtos) {
+        await new Promise(resolve => {
+          setTimeout(async () => {
+            await this.addProdutos(produto);
+            resolve();
+          }, 1000); 
+        });
+      }
+
+      res.status(200).json({ success: true, message: 'Produtos adicionados com sucesso.' });
+    } catch (error) {
+      console.error('Erro ao listar produtos:', error);
+      res.status(500).json({ success: false, message: 'Erro ao inserir produtos.' });
     }
-    console.log('Chaves', app_key, app_secret);
-    do {
-      const response = await axios.post('https://app.omie.com.br/api/v1/geral/produtos/', {
-        call: 'ListarProdutos',
-        app_key: app_key,
-        app_secret: app_secret,
-        param: [{
-          pagina: 1,
-          registros_por_pagina: 500,
-          apenas_importado_api: 'N',
-          filtrar_apenas_omiepdv: 'N'
-        }]
-      });
+  },
 
-      console.log('Chaves', app_key, app_secret);
-      const produtos = response.data.produto_servico_cadastro;
-      totalDeRegistros = response.data.total_de_registros;
-      todosProdutos = todosProdutos.concat(produtos);
-      
-      pagina++;
-    } while (todosProdutos.length < totalDeRegistros);
+  async listarProdutos({ cd_estabelecimento }, next) {
+    try {
+      let pagina = 1;
+      const registrosPorPagina = 1000;
+      let totalDeRegistros = 0;
+      let todosProdutos = [];
+      let app_key = "";
+      let app_secret = "";
 
-    return todosProdutos;
-  } catch (error) {
-    console.error('Erro ao listar produtos:', error);
-    return [];
-  }
-},
-async addProdutos(callback) {    
-    const produto1 = {
-      aliquota_cofins: produto.aliquota_cofins,
-      aliquota_ibpt: produto.aliquota_ibpt,
-      aliquota_icms: produto.aliquota_icms,
-      aliquota_pis: produto.aliquota_pis,
-      altura: produto.altura,
-      bloqueado: produto.bloqueado,
-      bloquear_exclusao: produto.bloquear_exclusao,
-      cest: produto.cest,
-      cfop: produto.cfop,
-      codInt_familia: produto.codInt_familia,
-      codigo: produto.codigo,
-      codigo_beneficio: produto.codigo_beneficio,
-      codigo_familia: produto.codigo_familia,
-      codigo_produto: produto.codigo_produto,
-      codigo_produto_integracao: produto.codigo_produto_integracao,
-      csosn_icms: produto.csosn_icms,
-      cst_cofins: produto.cst_cofins,
-      cst_icms: produto.cst_icms,
-      cst_pis: produto.cst_pis,
-      descr_detalhada: produto.descr_detalhada,
-      descricao: produto.descricao,
-      descricao_familia: produto.descricao_familia,
-      dias_crossdocking: produto.dias_crossdocking,
-      dias_garantia: produto.dias_garantia,
-      ean: produto.ean,
-      estoque_minimo: produto.estoque_minimo,
-      exibir_descricao_nfe: produto.exibir_descricao_nfe,
-      exibir_descricao_pedido: produto.exibir_descricao_pedido,
-      importado_api: produto.importado_api,
-      inativo: produto.inativo,
-      largura: produto.largura,
-      lead_time: produto.lead_time,
-      marca: produto.marca,
-      modelo: produto.modelo,
-      motivo_deson_icms: produto.motivo_deson_icms,
-      ncm: produto.ncm,
-      obs_internas: produto.obs_internas,
-      per_icms_fcp: produto.per_icms_fcp,
-      peso_bruto: produto.peso_bruto,
-      peso_liq: produto.peso_liq,
-      profundidade: produto.profundidade,
-      quantidade_estoque: produto.quantidade_estoque,
-      red_base_cofins: produto.red_base_cofins,
-      red_base_icms: produto.red_base_icms,
-      red_base_pis: produto.red_base_pis,
-      tipoItem: produto.tipoItem,
-      unidade: produto.unidade,
-      valor_unitario: produto.valor_unitario
-     };
+      if (cd_estabelecimento === 15) {
+        app_key = process.env.ApiKey_SP;
+        app_secret = process.env.ApiSecret_SP;
+      } else if (cd_estabelecimento === 5) {
+        app_key = process.env.ApiKey_ES;
+        app_secret = process.env.ApiSecret_ES;
+      } else {
+        app_key = process.env.ApiKey_RJ;
+        app_secret = process.env.ApiSecret_RJ;
+      }
+
+      do {
+        const response = await axios.post('https://app.omie.com.br/api/v1/geral/produtos/', {
+          call: 'ListarProdutos',
+          app_key: app_key,
+          app_secret: app_secret,
+          param: [{
+            pagina,
+            registros_por_pagina: registrosPorPagina,
+            apenas_importado_api: 'N',
+            filtrar_apenas_omiepdv: 'N'
+          }]
+        });
+
+        const produtos = response.data.produto_servico_cadastro;
+        totalDeRegistros = response.data.total_de_registros;
+        todosProdutos = todosProdutos.concat(produtos);
+        
+        pagina++;
+      } while (todosProdutos.length < totalDeRegistros);
+
+      return todosProdutos;
+    } catch (error) {
+      console.error('Erro ao listar produtos:', error);
+      next(error);
+    }
+  },
+
+  async addProdutos(produto) {
+    const {
+      aliquota_cofins,
+      aliquota_ibpt,
+      aliquota_icms,
+      aliquota_pis,
+      altura,
+      bloqueado,
+      bloquear_exclusao,
+      cest,
+      cfop,
+      codInt_familia,
+      codigo,
+      codigo_beneficio,
+      codigo_familia,
+      codigo_produto,
+      codigo_produto_integracao,
+      csosn_icms,
+      cst_cofins,
+      cst_icms,
+      cst_pis,
+      descr_detalhada,
+      descricao,
+      descricao_familia,
+      dias_crossdocking,
+      dias_garantia,
+      ean,
+      estoque_minimo,
+      exibir_descricao_nfe,
+      exibir_descricao_pedido,
+      importado_api,
+      inativo,
+      largura,
+      lead_time,
+      marca,
+      modelo,
+      motivo_deson_icms,
+      ncm,
+      obs_internas,
+      per_icms_fcp,
+      peso_bruto,
+      peso_liq,
+      profundidade,
+      quantidade_estoque,
+      red_base_cofins,
+      red_base_icms,
+      red_base_pis,
+      tipoItem,
+      unidade,
+      valor_unitario
+    } = produto;
 
     let transaction;
     try {
       transaction = await db.sequelize.transaction();          
-      const produto = await db.produtos.create({
+      await db.produtos.create({
         aliquota_cofins,
         aliquota_ibpt,
         aliquota_icms,
@@ -195,51 +195,46 @@ async addProdutos(callback) {
         valor_unitario
       }, { transaction });
 
-       await transaction.commit();  
-  
-      if (produto) {
-        return res.status(200).json({ success: true, msg: "Produto cadastrado com sucesso." });
-      } else {
-        res.status(500).json({ success: false });
-      }
+      await transaction.commit();
     } catch (err) {
       console.error(err);
       if (transaction) await transaction.rollback();
-      next(err);
-    }
-   },
-
-  
-   async getAllProdutos(req, res, next) {
-    try {
-        const produtos = await db.produtos.findAll({ paranoid: false });
-        
-        if (produtos) {
-            return res.status(200).json({ success: true, data: produtos });
-        } else {
-            return res.status(404).json({ success: false, message: 'Nenhum produto encontrado.' });
-        }
-    } catch (err) {
-        console.log(err);
-        next(err);
+      throw err;
     }
   },
-    async getallprodutosid(req,res,next){
-        const { ean } = req.body;
-        db.produtos.findOne({ where: { ean: ean }, paranoid: false })
-        .then(produtos => {
-            if (produtos) {
-                return res.status(200).json({ success: true, data:produtos});
-            }
-            else
-                res.status(500).json({ 'success': false });
-        })
-        .catch(err => {
-            console.log(err)
-            next(err);
-        })
-    },   
-}
+
+  async getAllProdutos(req, res, next) {
+    try {
+      const produtos = await db.produtos.findAll({ paranoid: false });
+      
+      if (produtos) {
+        return res.status(200).json({ success: true, data: produtos });
+      } else {
+        return res.status(404).json({ success: false, message: 'Nenhum produto encontrado.' });
+      }
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  },
+
+  async getallprodutosid(req, res, next) {
+    const { ean } = req.body;
+    try {
+      const produto = await db.produtos.findOne({ where: { ean }, paranoid: false });
+      
+      if (produto) {
+        return res.status(200).json({ success: true, data: produto });
+      } else {
+        return res.status(500).json({ success: false, message: 'Produto não encontrado.' });
+      }
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  },
+};
+
 
 
 
