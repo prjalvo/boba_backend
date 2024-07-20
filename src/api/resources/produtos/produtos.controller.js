7f3bbb4162ee33d7887a3c60575575724d31689f
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-07-20 17:35:32 -0300

    Update produtos.controller.js

--- src/api/resources/produtos/produtos.controller.js
+++ src/api/resources/produtos/produtos.controller.js
@@ -13,7 +13,7 @@ export default {
   async InserirProdutos(req, res, next) {
      try {
       let pagina = 1;
-      const registrosPorPagina = 1000;
+      const registrosPorPagina = 50;
       let totalDeRegistros = 0;
       let todosProdutos = [];
       let app_key=""
