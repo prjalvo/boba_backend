3ce11fff4198ce984368811a97476b0b3713d81e
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-08-03 09:28:22 -0300

    Update filiais.router.js

--- src/api/resources/filiais/filiais.router.js
+++ src/api/resources/filiais/filiais.router.js
@@ -7,6 +7,7 @@ import { validateBody, schemas } from '../../../middleware/validator.js';
 export const filiaisRouter = express.Router();
 filiaisRouter.route('/register').post(filiaisController.addFiliais);
 filiaisRouter.route('/getAllFiliais').get(sanitize(), jwtStrategy, filiaisController.getAllFiliais);
+filiaisRouter.route('/getFilialid').get(sanitize(), jwtStrategy, filiaisController.getFilialid);
 filiaisRouter.route('/update').post(jwtStrategy, filiaisController.filiaisUpdate);
 filiaisRouter.route('/delete').post(sanitize(), jwtStrategy, filiaisController.deleteFiliais);
 
