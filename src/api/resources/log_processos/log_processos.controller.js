02d4acc22b0677655aa608f512688d49586ef954
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-08-12 20:52:02 -0300

    Update log_processos.controller.js

--- src/api/resources/log_processos/log_processos.controller.js
+++ src/api/resources/log_processos/log_processos.controller.js
@@ -27,15 +27,15 @@ export default {
         res.status(200).json({ success: true, data: log_processos });
     } catch (err) {
         // Em caso de erro, retorna uma resposta de erro
-        console.log(err);
+        res.status(400).json({ 'success': false,'erro:' err });
         next(err);
     }
 },
-     async getAllFiliais(req,res,next){
-        db.filiais.findAll({ })
-        .then(filiais => {
-            if (filiais) {
-                return res.status(200).json({ success: true, data:filiais});
+     async getlog_processos(req,res,next){
+        db.log_processos.findAll({ })
+        .then(log_processos => {
+            if (log_processos) {
+                return res.status(200).json({ success: true, data:log_processos});
             }
             else
                 res.status(500).json({ 'success': false });
@@ -46,49 +46,5 @@ export default {
         })
     },
 
-     async filiaisUpdate(req,res,next){
-        const { nome, depositante,cd_estabelecimento,api_key,api_secret,logistica,processar,cnpj } = req.body;
-        
-        db.filiais.findOne({ where: { cd_estabelecimento: id }, paranoid: false })
-            .then(user => {
-                if (!user) {
-                    throw new RequestError('User is not found', 409);
-                }
-                return db.filiais.update({
-                    nome: nome ? nome : filiais.nome,                    
-                    depositante: depositante ? depositante : filiais.depositante,                      
-                    cd_estabelecimento: cd_estabelecimento ? cd_estabelecimento : filiais.cd_estabelecimento,
-                    api_key: api_key ? api_key : filiais.api_key,
-                    api_secret: api_secret ? api_secret : filiais.api_secret,
-                    logistica: logistica ? logistica : filiais.logistica,
-                    processar: processar ? processar : filiais.processar,                    
-                    cnpj: cnpj ? cnpj : filiais.cnpj                     
-                }, { where: { id: id } })
-            })
-            .then(user => {
-                if (user) {
-                    return res.status(200).json({ success: true, msg: "User update successsfully"});
-                }
-                else
-                    res.status(500).json({ 'success': false });
-            })
-            .catch(err => {
-                console.log(err)
-                next(err);
-            })
-    },
-      async deleteFiliais(req, res, next) {
-        db.filiais.findOne({ where: { cd_estabelecimento: req.body.id} })
-            .then(data => {
-                if (data) {
-                    return db.filiais.destroy({ where: { cd_estabelecimento: req.body.id } }).then(r => [r, data])
-                }
-                throw new RequestError('User is not found', 409)
-            })
-            .then(re => {
-                return res.status(200).json({ 'status': "deleted userlist Seccessfully" });
-            }).catch(err => {
-                next(err)
-            })
-    }
+    
 }
