39c7fc6ac4f0b0becd94633d5ff0444a4d57f4f0
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-08-12 20:09:24 -0300

    Update log_processos.router.js

--- src/api/resources/log_processos/log_processos.router.js
+++ src/api/resources/log_processos/log_processos.router.js
@@ -5,7 +5,7 @@ import { sanitize } from '../../../middleware/sanitizer.js';
 import { validateBody, schemas } from '../../../middleware/validator.js';
 
 export const log_processosRouter = express.Router();
-log_processosRouter.route('/register').post(log_processosController.);
+log_processosRouter.route('/register').post(log_processosController.addlog_processos);
 log_processosRouter.route('/getAllFiliais').get(sanitize(), jwtStrategy, log_processosController.getAllFiliais);
 log_processosRouter.route('/update').post(jwtStrategy, log_processosController.filiaisUpdate);
 log_processosRouter.route('/delete').post(sanitize(), jwtStrategy, log_processosController.deleteFiliais);
