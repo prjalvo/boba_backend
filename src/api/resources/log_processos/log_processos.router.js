e3f69005ffb3d365b8350afe1286274c6cd4982a
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-08-12 20:08:57 -0300

    Update log_processos.router.js

--- src/api/resources/log_processos/log_processos.router.js
+++ src/api/resources/log_processos/log_processos.router.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import filiaisController from './filiais.controller.js';
+import log_processosController from './log_processos.controller.js';
 import { localStrategy , jwtStrategy} from '../../../middleware/strategy.js';
 import { sanitize } from '../../../middleware/sanitizer.js';
 import { validateBody, schemas } from '../../../middleware/validator.js';
 
-export const filiaisRouter = express.Router();
-filiaisRouter.route('/register').post(filiaisController.addFiliais);
-filiaisRouter.route('/getAllFiliais').get(sanitize(), jwtStrategy, filiaisController.getAllFiliais);
-filiaisRouter.route('/update').post(jwtStrategy, filiaisController.filiaisUpdate);
-filiaisRouter.route('/delete').post(sanitize(), jwtStrategy, filiaisController.deleteFiliais);
+export const log_processosRouter = express.Router();
+log_processosRouter.route('/register').post(log_processosController.);
+log_processosRouter.route('/getAllFiliais').get(sanitize(), jwtStrategy, log_processosController.getAllFiliais);
+log_processosRouter.route('/update').post(jwtStrategy, log_processosController.filiaisUpdate);
+log_processosRouter.route('/delete').post(sanitize(), jwtStrategy, log_processosController.deleteFiliais);
 
 
