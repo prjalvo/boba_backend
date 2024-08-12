965ffa6584ff9387990484f7097837f2e5fe9c39
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-08-12 20:24:19 -0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { authRouter } from './resources/auth/index.js'
 import { filiaisRouter } from './resources/filiais/index.js'
 import { produtosRouter } from './resources/produtos/index.js'
+import { log_processosRouter } from './resources/log_processos/index.js'
  
 export const restRouter = express.Router();
 restRouter.use('/auth', authRouter);
 restRouter.use('/filiais', filiaisRouter);
 restRouter.use('/produtos', produtosRouter);
+restRouter.use('/log_processos', log_processosRouter);
 
 
