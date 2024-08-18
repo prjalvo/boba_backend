3912d5c36bd66512c737c1a845f1b9e334d6d860
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-08-18 10:41:09 -0300

    Create Controle_proces_ent.cjs

---
+++ src/models/Controle_proces_ent.cjs
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const controle_proces_ent = sequelize.controle_proces_ent', {
+    id: {type: DataTypes.INTEGER,primaryKey: true},
+    nnf: DataTypes.STRING,
+    status: DataTypes.STRING,    
+    cd_estabelecimento: DataTypes.STRING,
+    etapa: DataTypes.STRING,
+    createdat: DataTypes.DATE, 
+    demissaonfe: DataTypes.DATE    
+  }, {
+      timestamps: false,  // Desabilita o gerenciamento automático de timestamps
+      createdAt: 'createdat',  // Nome do campo para a data de criação
+      tableName: 'controle_proces_ent'
+  });
+
+  controle_proces_ent.associate = function(models) {      
+  };
+  return controle_proces_ent;
+};
