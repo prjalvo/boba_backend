0e4ac8cb28c472b5bdf672fe4f290b5088a0fa6b
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-08-18 11:01:24 -0300

    Update log_processos.router.js

--- src/api/resources/log_processos/log_processos.router.js
+++ src/api/resources/log_processos/log_processos.router.js
@@ -11,4 +11,6 @@ log_processosRouter.route('/getlog_processos').get(log_processosController.getlo
 log_processosRouter.route('/addCTRL').post(log_processosController.addCTRL);
 log_processosRouter.route('/getCTRL').get(log_processosController.getCTRL);
 
+log_processosRouter.route('/CTRLUpdate').post(log_processosController.CTRLUpdate);
+log_processosRouter.route('/deleteCTRL').get(log_processosController.deleteCTRL);
 
