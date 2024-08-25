ffc979bf0e23e6787bdc77c8bb46b8fde913dced
Everson Rocha <63676612+prjalvo@users.noreply.github.com>
2024-08-25 11:18:35 -0300

    Update Produtos.cjs

--- src/models/Produtos.cjs
+++ src/models/Produtos.cjs
@@ -175,7 +175,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   Produtos.associate = function(models) {
-   Produtos.controle_proces_ent.belongsTo(models.filiais, { foreignKey: 'cd_estabelecimento' });
+   Produtos.belongsTo(models.filiais, { foreignKey: 'cd_estabelecimento' });
   };
 
   return Produtos;
